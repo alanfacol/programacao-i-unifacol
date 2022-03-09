5e56b9d34950d2a680f44f5bdc0eb5a58a208f60
alanmts-am <alanmts.am@gmail.com>
2022-03-09 20:17:09 -0300

    Add aula03

---
+++ src/aulas/a03/Main.java
@@ -0,0 +1,55 @@
+package aulas.a03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String cor_semaforo;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe a cor do semaforo");
+        cor_semaforo = scan.nextLine();
+        scan.close();
+
+        // Jeito 01
+        // if (cor_semaforo.equals("vermelho")){
+        //     System.out.println("A cor escolhida é " + cor_semaforo);
+        // }
+        // if (cor_semaforo.equals("amarelo")){
+        //     System.out.println("A cor escolhida é " + cor_semaforo);
+        // }
+        // if (cor_semaforo.equals("verde")){
+        //     System.out.println("A cor escolhida é " + cor_semaforo);
+        // }
+        // else {
+        //     System.out.println("A cor informada não bate com as cores de um semaforo");
+        // }
+
+        // Jeito 02
+        // if (cor_semaforo.equals("vermelho")){
+        //     System.out.println("A cor escolhida é " + cor_semaforo);
+        // } else {
+        //     if (cor_semaforo.equals("amarelo")){
+        //         System.out.println("A cor escolhida é " + cor_semaforo);
+        //     } else {
+        //         if (cor_semaforo.equals("verde")){
+        //             System.out.println("A cor escolhida é " + cor_semaforo);
+        //         } else {
+        //             System.out.println("A cor informada não bate com as cores de um semaforo");
+        //         }
+        //     }
+        // }
+
+        // Jeito 03
+        if (cor_semaforo.equals("vermelho")){
+            System.out.println("A cor escolhida é " + cor_semaforo);
+        } else if (cor_semaforo.equals("amarelo")){
+            System.out.println("A cor escolhida é " + cor_semaforo);
+        }else if (cor_semaforo.equals("verde")){
+            System.out.println("A cor escolhida é " + cor_semaforo);
+        } else {
+            System.out.println("A cor informada não bate com as cores de um semaforo");
+        }
+    }
+}
