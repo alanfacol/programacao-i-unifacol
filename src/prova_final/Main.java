00feebd193d8155785b48a3e4075796037c8d038
alanmts-am <alanmts.am@gmail.com>
2022-05-25 13:58:03 -0300

    Add main class

---
+++ src/prova_final/Main.java
@@ -0,0 +1,53 @@
+package prova_final;
+
+import java.util.ArrayList;
+
+import prova_final.entidades.Tecnico;
+import prova_final.entidades.Jogador;
+import prova_final.entidades.Time;
+import prova_final.enums.EPosicao;
+import prova_final.servicos.TimeServico;
+
+public class Main {
+    public static void main(String[] args) {
+        // Criação dos Jogadores
+        ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
+        jogadores.add(new Jogador("Alan", "11111111111", EPosicao.ATACANTE));
+        jogadores.add(new Jogador("Bruno", "22222222222", EPosicao.MEIA));
+        jogadores.add(new Jogador("Cesar", "33333333333", EPosicao.DEFENSOR));
+        jogadores.add(new Jogador("Daniel", "44444444444", EPosicao.GOLEIRO));
+
+        // Criação do técnico
+        Tecnico tecnico = new Tecnico();
+        tecnico.setNome("Lazaro do Sertão");
+
+        // Criação do time
+        Time t1 = new Time();
+        t1.setNome("Sport");
+        t1.setJogadores(jogadores);
+        t1.setTecnico(tecnico);
+        t1.setVitorias(0);
+        t1.setEmpates(0);
+        t1.setDerrotas(0);
+
+        // Chamada do serviço
+        TimeServico timeServico = new TimeServico();
+        timeServico.cadastrar(t1);
+
+        // Alterando o nome do time pelo seu objeto
+        ArrayList<Time> timesBusca = timeServico.pesquisar("Sport");
+        for (Time time : timesBusca) {
+            time.setNome("Sport Recife");
+            timeServico.alterar(time);
+            break;
+        }
+
+        // Listando os times
+        ArrayList<Time> teste = timeServico.listarTodos();
+
+        for (Time time : teste) {
+            System.out.println(time.getNome());
+            System.out.println(time.getTecnico().getNome());
+        }
+    }
+}
