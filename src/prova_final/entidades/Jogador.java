21b20e49da2c8d355083246195846a29a334cd55
alanmts-am <alanmts.am@gmail.com>
2022-05-25 13:57:35 -0300

    Add constructors

--- src/prova_final/entidades/Jogador.java
+++ src/prova_final/entidades/Jogador.java
@@ -2,21 +2,34 @@ package prova_final.entidades;
 
 import java.util.ArrayList;
 
-import atividades.entidades.voleibol.enums.EPosicao;
+import prova_final.enums.EPosicao;
 
 public class Jogador extends Atleta {
     private EPosicao posicao;
     private ArrayList<Cartao> cartoes;
-    
+
+    public Jogador() {
+    }
+
+    public Jogador(String nome, String cpf, EPosicao posicao) {
+        this.setNome(nome);
+        this.setCpf(cpf);
+        this.setPosicao(posicao);
+        this.cartoes = null;
+    }
+
     public EPosicao getPosicao() {
         return posicao;
     }
+
     public void setPosicao(EPosicao posicao) {
         this.posicao = posicao;
     }
+
     public ArrayList<Cartao> getCartoes() {
         return cartoes;
     }
+
     public void setCartoes(ArrayList<Cartao> cartoes) {
         this.cartoes = cartoes;
     }
