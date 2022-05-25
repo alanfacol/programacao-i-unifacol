3f82e10c341f1ba1e67105f0fbfac4f8f46baab4
alanmts-am <alanmts.am@gmail.com>
2022-05-25 13:56:49 -0300

    Implement methods

--- src/prova_final/repositorios/TimeRepositorio.java
+++ src/prova_final/repositorios/TimeRepositorio.java
@@ -6,30 +6,41 @@ import prova_final.entidades.Time;
 import prova_final.interfaces.ITimeRepo;
 
 public class TimeRepositorio implements ITimeRepo {
+    private ArrayList<Time> timeRepo = new ArrayList<Time>();
 
     @Override
     public void cadastrar(Time time) {
-        
+        timeRepo.add(time);
     }
 
     @Override
     public void alterar(Time time) {
-        
+        for (Time t : timeRepo) {
+            if (t.equals(time)){
+                timeRepo.set(timeRepo.lastIndexOf(t), time);
+            }
+        }
     }
 
     @Override
     public void deletar(Time time) {
-        
+        timeRepo.remove(time);
     }
 
     @Override
     public ArrayList<Time> pesquisar(String nome) {
-        return null;
+        ArrayList<Time> times = new ArrayList<Time>();
+        for (Time time : timeRepo) {
+            if (time.getNome().equals(nome)){
+                times.add(time);
+            }
+        }
+        return times;
     }
 
     @Override
     public ArrayList<Time> listarTodos() {
-        return null;
+        return timeRepo;
     }
-    
+
 }
--- src/prova_final/servicos/TimeServico.java
+++ src/prova_final/servicos/TimeServico.java
@@ -4,27 +4,35 @@ import java.util.ArrayList;
 
 import prova_final.entidades.Time;
 import prova_final.interfaces.ITimeServico;
+import prova_final.repositorios.TimeRepositorio;
 
 public class TimeServico implements ITimeServico {
 
+    private TimeRepositorio timeRepo = new TimeRepositorio();
+
     @Override
     public void cadastrar(Time time) {
-        
+        timeRepo.cadastrar(time);
     }
 
     @Override
     public void alterar(Time time) {
-        
+        timeRepo.alterar(time);
     }
 
     @Override
     public void deletar(Time time) {
-        
+        timeRepo.deletar(time);
     }
 
     @Override
     public ArrayList<Time> pesquisar(String nome) {
-        return null;
+        return timeRepo.pesquisar(nome);
+    }
+
+    @Override
+    public ArrayList<Time> listarTodos() {
+        return timeRepo.listarTodos();
     }
     
 }
